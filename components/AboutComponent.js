0e1e9e3e0298cffcf88e95b8434262bdc5e16994
Shane Burke <shaneburkework@gmail.com>
2021-11-17 13:07:36 -1000

    Using Redux in React Native

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import { FlatList, ScrollView, Text } from 'react-native';
 import { Card, ListItem } from "react-native-elements";
 import { PARTNERS } from "../shared/partners";
+import { connect } from 'react-redux';
+import { baseUrl } from "../shared/baseUrl"
+
+
+const MapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 
 function Mission () {
@@ -17,13 +26,6 @@ function Mission () {
 
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
-
     static NavigationOptions ={
         title: "About"
     }
@@ -35,7 +37,7 @@ class About extends Component {
                 <ListItem 
                 title = {item.title}
                 subtitle = {item.description}
-                leftAvatar = {{ source: require('./images/bootstrap-logo.png')}}
+                leftAvatar = {{ source: {uri: baseUrl + item.image}}}
                 />
             )
         }
@@ -45,7 +47,7 @@ class About extends Component {
             <Mission />
             <Card Title={"Community Partners"}>
                 <FlatList 
-                    data={this.state.partners}
+                    data={this.props.partners.partners}
                     renderItem={renderPartner}
                     keyExtractor={item=>item.id.toString()}
                 />
@@ -56,4 +58,4 @@ class About extends Component {
 }
 
 
-export default About;
+export default connect(MapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,15 @@ import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 
 
@@ -233,6 +242,14 @@ const styles = StyleSheet.create({
 const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
+
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+
     render() {
         return (
             <View style={{
@@ -245,4 +262,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
